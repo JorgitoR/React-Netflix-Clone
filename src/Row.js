7d4f2f4d1b252b939ac3a2e6c570eb9638f26255
JorgitoR <jorgitouribe133@gmail.com>
2021-05-14 09:28:17 -0500

    React Netflix-Clone

--- src/Row.js
+++ src/Row.js
@@ -1,13 +1,43 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from './axios';
 
-function Row({title}){
 
-	const [moviews, setMovies] = useState([]);
+const base_url = 'https://image.tmdb.org/t/p/original/';
+
+function Row({title, fetchUrl}){
+
+	const [movies, setMovies] = useState([]);
+
+	useEffect(()=> {
+
+		async function fetchData(){
+			const request = await axios.get(fetchUrl);
+			console.log(request.data.results);
+			setMovies(request.data.results)
+			return request;
+
+		}
+		fetchData();
+	}, [fetchUrl]);
+
+	console.table(movies)
 
 	return (
 
-		<div>
+		<div className='row'>
 			<h2>{title}</h2>
+			<div className='row_posters'>
+			{/*Poster*/}
+
+			{movies.map(movie =>(
+				
+				<img 
+					className='row__poster'
+					src={`${base_url}${movie.poster_path}`} 
+					alt={movie.name}/>
+			))}
+
+			</div>
 		</div>
 	)
 
