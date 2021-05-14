f34722d3f3210fd9270e1adfd1c1f080ced25ecf
JorgitoR <jorgitouribe133@gmail.com>
2021-05-14 10:12:58 -0500

    React Netflix-Clone

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 
 import Row from './Row';
 import request from './request';
+import Banner from '/.Banner';
 
 function App() {
   return (
     <div className="App">
 
-      <Row title='NETFLIX ORIGINALS' fetchUrl={request.fetchNetflixOriginals} />
+      <Banner />
+
+      <Row title='NETFLIX ORIGINALS' fetchUrl={request.fetchNetflixOriginals} isLargeRow={true} />
       <Row title='Trending Now' fetchUrl={request.fetchTrending} />
       <Row title='Top Rated' fetchUrl={request.fetchTopRated} />
       <Row title='Action Movies' fetchUrl={request.fetchActionMovies} />
