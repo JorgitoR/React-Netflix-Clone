64bc8aa4c45ed900fcff9b6c39c570325a7a92a8
JorgitoR <jorgitouribe133@gmail.com>
2021-05-15 09:33:18 -0500

    React Netflix

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 import Row from './Row';
 import request from './request';
 import Banner from './Banner';
+import Nav from './Nav';
 
 function App() {
   return (
     <div className="App">
 
+      <Nav />
       <Banner />
 
       <Row title='NETFLIX ORIGINALS' fetchUrl={request.fetchNetflixOriginals} isLargeRow={true} />
