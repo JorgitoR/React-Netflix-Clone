e406a2cf6a1c7a1a761d65ea66fac5c9a0380915
JorgitoR <jorgitouribe133@gmail.com>
2021-05-15 07:31:16 -0500

    React Netflix

--- src/Banner.js
+++ src/Banner.js
@@ -45,6 +45,12 @@ function Banner(){
 				<h1 className='banner_title'> 
 					{{movie?.title || movie?.name || movie?.original_name}}
 				</h1>
+
+				<div className="banner__buttons">
+					<button className="banner__button">Play</button>
+					<button className="banner__button">My List</button>
+				</div>
+
 			</div>
 			{/*Background Image*/}
 			{/*titulo*/}
