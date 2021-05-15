d6b179c9594c4310016effef3f2f3bab4c8ada91
JorgitoR <jorgitouribe133@gmail.com>
2021-05-15 09:38:23 -0500

    React Netflix

--- src/Nav.js
+++ src/Nav.js
@@ -1,2 +1,29 @@
 import React, {useState, useEffect} from 'react'
 import './Nav.css';
+
+function Nav(){
+
+	const [show, handleShow] = useState(false);
+
+	//when scroll is a 100px (down in page) we add the navbar visibility
+	//if not remove visibility on navbar
+
+
+	useEffect(()=>{
+		window.addEventListener("scroll", ()=>{
+			if(window.scrollY > 100){
+				handleShow(true);
+			}
+			else handleShow(false);
+		});
+		return ()=> {
+			window.removeEventListener("scroll");
+		};	
+	}, []);
+
+	return (
+
+
+	)
+
+}
